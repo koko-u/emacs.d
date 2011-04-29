2c793004bc9a8351f1fd0dbfc1f4cf238cc25066
Kozaki, Tsuneaki <kozaki.tsuneaki@gmail.com>
2011-04-30 03:43:46 +0900

    Ubuntu を 11.04 にバージョンアップしたら、フォントがおかしくなったので修正

--- init.el
+++ init.el
@@ -134,3 +134,17 @@
             (byte-recompile-directory (concat user-emacs-directory "local-lisp") 0)
             (byte-recompile-directory (concat user-emacs-directory "site-start.d") 0)
             ))
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(cua-mode t nil (cua-base))
+ '(show-paren-mode t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "unknown" :family "Takaoゴシック")))))
