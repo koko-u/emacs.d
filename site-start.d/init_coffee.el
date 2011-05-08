02fa969f0882aa0a8aee325e4c449e276aee21ab
Kozaki, Tsuneaki <kozaki.tsuneaki@gmail.com>
2011-05-08 18:35:42 +0900

    coffee-mode に flymake の設定を追加。参考 : http://d.hatena.ne.jp/antipop/20110508/1304838383

--- site-start.d/init_coffee.el
+++ site-start.d/init_coffee.el
@@ -45,6 +45,33 @@
 (require 'shadow)
 (add-hook 'find-file-hook 'shadow-on-find-file)
 
+;; flymake
+(defvar flymake-coffeescript-err-line-patterns
+  '(("\\(Error: In \\([^,]+\\), .+ on line \\([0-9]+\\).*\\)" 2 3 nil 1)))
+
+(defconst flymake-allowed-coffeescript-file-name-masks
+  '(("\\.coffee$" flymake-coffeescript-init)))
+
+(defun flymake-coffeescript-init ()
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                     'flymake-create-temp-inplace))
+         (local-file (file-relative-name
+                      temp-file
+                      (file-name-directory buffer-file-name))))
+    (list "coffee" (list local-file))))
+
+(defun flymake-coffeescript-load ()
+  (interactive)
+  (defadvice flymake-post-syntax-check (before flymake-force-check-was-interrupted)
+    (setq flymake-check-was-interrupted t))
+  (ad-activate 'flymake-post-syntax-check)
+  (setq flymake-allowed-file-name-masks
+        (append flymake-allowed-file-name-masks
+                flymake-allowed-coffeescript-file-name-masks))
+  (setq flymake-err-line-patterns flymake-coffeescript-err-line-patterns)
+  (flymake-mode t))
+
+(add-hook 'coffee-mode-hook 'flymake-coffeescript-load)
 
 (provide 'init_coffee)
 ;;; init_ac.el ends here
