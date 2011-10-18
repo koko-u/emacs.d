43e4132542eee7f8202a765647baef822eaf5ca3
Kozaki, Tsuneaki <kozaki.tsuneaki@gmail.com>
2011-10-19 00:29:36 +0900

    remove server-done-hook
    version up Ubuntu 11.04 to 11.10, i encountered freeze when git commit by emacsclient.

--- site-start.d/init_emacs-client.el
+++ site-start.d/init_emacs-client.el
@@ -25,12 +25,14 @@
 ;; ように改善します。
 
 ;;; Code:
+(require 'server)
+(unless (server-running-p)
+  (server-start))
 
-(server-start)
-(defun iconify-emacs-when-server-is-done ()
-  (unless server-clients (iconify-frame)))
+;(defun iconify-emacs-when-server-is-done ()
+;  (unless server-clients (iconify-frame)))
 
-(add-hook 'server-done-hook 'iconify-emacs-when-server-is-done)
+;(add-hook 'server-done-hook 'iconify-emacs-when-server-is-done)
 (global-set-key (kbd "C-x C-c") 'server-edit)
 (defalias 'exit 'save-buffers-kill-emacs)
 
