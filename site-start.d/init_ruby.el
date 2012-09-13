4906dcd4189e848947317279a435f573f3573535
Kozaki, Tsuneaki <kozaki.tsuneaki@gmail.com>
2012-09-14 00:03:05 +0900

    modify indent setting on ruby

--- site-start.d/init_ruby.el
+++ site-start.d/init_ruby.el
@@ -130,5 +130,32 @@ and source-file directory for your debugger" t)
 
 (require 'ffap)
 (add-to-list 'ffap-alist '(ruby-mode . ffap-ruby-mode))
+
+(setq ruby-deep-indent-paren-style nil)
+(defadvice ruby-indent-line (after unindent-closing-paren activate)
+  (let ((column (current-column))
+        indent offset)
+    (save-excursion
+      (back-to-indentation)
+      (let ((state (syntax-ppss)))
+        (setq offset (- column (current-column)))
+        (when (and (eq (char-after) ?\))
+                   (not (zerop (car state))))
+          (goto-char (cadr state))
+          (setq indent (current-indentation)))))
+    (when indent
+      (indent-line-to indent)
+      (when (> offset 0) (forward-char offset)))))
+
+;; rcodetools
+(require 'rcodetools)
+(require 'anything-rcodetools)
+(defun ruby-mode-hook-rcodetools ()
+  (define-key ruby-mode-map (kbd "C-c C-i") 'rct-complete-symbol)
+  (define-key ruby-mode-map (kbd "C-c C-t") 'ruby-toggle-buffer)
+  (define-key ruby-mode-map (kbd "C-c C-d") 'xmp)
+  (define-key ruby-mode-map (kbd "C-c C-f") 'rct-ri))
+(add-hook 'ruby-mode-hook 'ruby-mode-hook-rcodetools)
+
 (provide 'init_ruby)
 ;;; init_ruby.el ends here
