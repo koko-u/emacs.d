c41051092d5d958cd4597e57189aa0ecbfd354eb
Kozaki, Tsuneaki <kozaki.tsuneaki@gmail.com>
2012-09-14 19:15:46 +0900

    add groovy settings

---
+++ site-start.d/init_groovy.el
@@ -0,0 +1,36 @@
+;;; init_groovy.el ---
+
+;; Copyright (C) 2012  kozaki.tsuneaki
+
+;; Author: kozaki.tsuneaki <kozaki.tsuneaki@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code" t)
+(add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
+(add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
+(add-hook 'groovy-mode-hook
+          '(lambda ()
+             (require 'groovy-electric)
+             (groovy-electric-mode)))
+
+(provide 'init_groovy)
+;;; init_groovy.el ends here
