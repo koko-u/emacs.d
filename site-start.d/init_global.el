363576f0bd795ddf437e1f1bd48ef513c26584e8
Kozaki, Tsuneaki <kozaki.tsuneaki@gmail.com>
2013-01-27 00:08:58 +0900

    grep-edit を追加

--- site-start.d/init_global.el
+++ site-start.d/init_global.el
@@ -158,7 +158,23 @@
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 ;; 余分な行末の空白を削除する
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+;; auto-save の度に呼ばれると、今まさに挿入した空白まで削除
+;; されてしまうので、現在行を除いてトリムするよう変更
+;; http://stackoverflow.com/questions/3533703/emacs-delete-trailing-whitespace-except-current-line
+(defun delete-trailing-whitespace-except-current-line ()
+  (interactive)
+  (let ((begin (line-beginning-position))
+        (end   (line-end-position)))
+    (save-excursion
+      (when (< (point-min) begin)
+        (save-restriction
+          (narrow-to-region (point-min) (1- begin))
+          (delete-trailing-whitespace)))
+      (when (> (point-max) end)
+        (save-restriction
+          (narrow-to-region (1+ end) (point-max))
+          (delete-trailing-whitespace))))))
+(add-hook 'before-save-hook 'delete-trailing-whitespace-except-current-line)
 
 ;; タイトルバーにファイルのフルパスを表示
 (setq frame-title-format
@@ -184,6 +200,25 @@
 ;; color-moccur
 (require 'color-moccur)
 (load "moccur-edit")
+
+;; grep-edit
+(require 'grep)
+(require 'grep-edit)
+
+(defadvice grep-edit-change-file (around inhibit-read-only activate)
+  ""
+  (let ((inhibit-read-only t))
+    ad-do-it))
+;; (progn (ad-disable-advice 'grep-edit-change-file 'around 'inhibit-read-only) (ad-update 'grep-edit-change-file))
+
+(defun my-grep-edit-setup ()
+  (define-key grep-mode-map '[up] nil)
+  (define-key grep-mode-map "\C-c\C-c" 'grep-edit-finish-edit)
+  (message (substitute-command-keys "\\[grep-edit-finish-edit] to apply changes."))
+  (set (make-local-variable 'inhibit-read-only) t)
+  )
+(add-hook 'grep-setup-hook 'my-grep-edit-setup t)
+
 
 ;; 安全な実行のための共通系関数
 
