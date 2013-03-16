9af924979fc9a631229909c296851aff0060acad
Kozaki, Tsuneaki <kozaki.tsuneaki@gmail.com>
2013-03-16 11:46:54 +0900

    add automatically indent after paste

--- site-start.d/init_c.el
+++ site-start.d/init_c.el
@@ -119,5 +119,19 @@
                       (fset 'makefile-warn-suspicious-lines 'ignore)
                       (setq indent-tabs-mode t))))
 
+;; コードを貼り付けた時に自動的にインデントする
+(dolist (command '(yank yank-pop))
+       (eval `(defadvice ,command (after indent-region activate)
+                (and (not current-prefix-arg)
+                     (member major-mode '(emacs-lisp-mode lisp-mode
+                                                          clojure-mode    scheme-mode
+                                                          haskell-mode    ruby-mode
+                                                          rspec-mode      python-mode
+                                                          c-mode          c++-mode
+                                                          objc-mode       latex-mode
+                                                          plain-tex-mode))
+                     (let ((mark-even-if-inactive transient-mark-mode))
+                       (indent-region (region-beginning) (region-end) nil))))))
+
 (provide 'init_c)
 ;;; init_c.el ends here
