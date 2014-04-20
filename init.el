a9158dab416ca9f6d475c45078037dc0182b91a9
Kozaki, Tsuneaki <kozaki.tsuneaki@gmail.com>
2014-04-21 05:56:06 +0900

    remove white space

--- init.el
+++ init.el
@@ -45,7 +45,7 @@
                   "auto-install"
                   ;; 初期設定ファイル
                   "site-start.d"
-                  ;; init-loader 
+                  ;; init-loader
                   "site-lisp"
                   )
 
@@ -195,10 +195,16 @@
 ;; 終了時バイトコンパイル
 (add-hook 'kill-emacs-query-functions
           (lambda ()
-            (if (file-newer-than-file-p (concat user-emacs-directory "init.el") 
+            (if (file-newer-than-file-p (concat user-emacs-directory "init.el")
                                         (concat user-emacs-directory "init.elc"))
                 (byte-compile-file (concat user-emacs-directory "init.el")))
             (byte-recompile-directory (concat user-emacs-directory "site-lisp") 0)
             (byte-recompile-directory (concat user-emacs-directory "site-start.d") 0)
             ))
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
