f6ecb2d0b8f1f6be0a9dfce32ab9e942b4b9941e
Kozaki, Tsuneaki <kozaki.tsuneaki@gmail.com>
2014-04-20 18:02:57 +0900

    setting emacsclient

--- inits/00-basic.el
+++ inits/00-basic.el
@@ -304,3 +304,15 @@
 ;;     (set-text-properties 0 (length new) nil new)
 ;;     ad-do-it))
 
+
+;; emacsclient
+(require 'server)
+(unless (server-running-p)
+  (server-start))
+
+;(defun iconify-emacs-when-server-is-done ()
+;  (unless server-clients (iconify-frame)))
+
+;(add-hook 'server-done-hook 'iconify-emacs-when-server-is-done)
+(global-set-key (kbd "C-x C-c") 'server-edit)
+(defalias 'exit 'save-buffers-kill-emacs)
