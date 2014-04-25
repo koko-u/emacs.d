522495c583690fd0d385eb89bab6e36503db1369
Kozaki, Tsuneaki <kozaki.tsuneaki@gmail.com>
2014-04-26 00:37:55 +0900

    introduce flycheck-pos-tip

--- inits/21-flycheck.el
+++ inits/21-flycheck.el
@@ -1,3 +1,29 @@
+;;; 21-flycheck.el --- flycheck
+
+;; Copyright (C) 2014  kozaki.tsuneaki
+
+;; Author: kozaki.tsuneaki <kozaki.tsuneaki@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
 (eval-after-load 'flycheck
@@ -5,10 +31,13 @@
 
 (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)
 
-(require 'flycheck-tip)
-
 (eval-after-load 'flycheck
   '(custom-set-variables
     '(flycheck-clang-language-standard "c++11")
-    '(flycheck-clang-include-path "/usr/include/c++/v1")
+    '(flycheck-clang-include-path '("/usr/include/c++/v1"))
+    '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)
+    '(flycheck-pos-tip-timeout 10)
     ))
+
+(provide '21-flycheck)
+;;; 21-flycheck.el ends here
