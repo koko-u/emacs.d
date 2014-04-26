690278ab533633e1fbe8aa7574401ee942ba94b8
Kozaki, Tsuneaki <kozaki.tsuneaki@gmail.com>
2014-04-26 12:39:09 +0900

    slightly change

--- inits/31-ccmode.el
+++ inits/31-ccmode.el
@@ -1,24 +1,50 @@
+;;; 31-ccmode.el --- cc-mode
+
+;; Copyright (C) 2014  kozaki.tsuneaki
+
+;; Author: kozaki.tsuneaki <kozaki.tsuneaki@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
 ;; スタイル設定
 (add-hook 'c-mode-common-hook
           '(lambda ()
-             ; styleには GNU,cc-mode,ktr,bsd,stroustrup,whitesmith
-             ; ,ellemtel,linux等がある
-             ;(c-set-style "ellemtel")
+                                        ; styleには GNU,cc-mode,ktr,bsd,stroustrup,whitesmith
+                                        ; ,ellemtel,linux等がある
+                                        ;(c-set-style "ellemtel")
              (c-set-style "cc-mode")
-             ; 基本オフセット
+                                        ; 基本オフセット
              (setq c-basic-offset 4)
-             ; コメント行のオフセット
-             ;(c-comment-only-line-of . 0)
-             ; 全自動インデントを有効
-             ;(c-toggle-auto-newline t)
-             ; TABキーでインデント
+                                        ; コメント行のオフセット
+                                        ;(c-comment-only-line-of . 0)
+                                        ; 全自動インデントを有効
+                                        ;(c-toggle-auto-newline t)
+                                        ; TABキーでインデント
              (setq c-tab-always-indent t)
-             ; namespace {}の中はインデントしない
+                                        ; namespace {}の中はインデントしない
              (c-set-offset 'innamespace 0)
-             ; 連続するスペースをバックスペース一回で削除する
+                                        ; 連続するスペースをバックスペース一回で削除する
              (c-toggle-hungry-state t)
-             ; センテンスの終了である ; を入力したら自動的に改行してインデント
-             ;(c-toggle-auto-hungry-state t)
+                                        ; センテンスの終了である ; を入力したら自動的に改行してインデント
+                                        ;(c-toggle-auto-hungry-state t)
              ;; 対応する括弧の挿入 しばらく smartchr を利用してみる
              ;; (make-variable-buffer-local 'skeleton-pair)
              ;; (make-variable-buffer-local 'skeleton-pair-on-word)
@@ -45,14 +71,17 @@
 
 ;; コードを貼り付けた時に自動的にインデントする
 (dolist (command '(yank yank-pop))
-       (eval `(defadvice ,command (after indent-region activate)
-                (and (not current-prefix-arg)
-                     (member major-mode '(emacs-lisp-mode lisp-mode
-                                                          clojure-mode    scheme-mode
-                                                          haskell-mode    ruby-mode
-                                                          rspec-mode      python-mode
-                                                          c-mode          c++-mode
-                                                          objc-mode       latex-mode
-                                                          plain-tex-mode))
-                     (let ((mark-even-if-inactive transient-mark-mode))
-                       (indent-region (region-beginning) (region-end) nil))))))
+  (eval `(defadvice ,command (after indent-region activate)
+           (and (not current-prefix-arg)
+                (member major-mode '(emacs-lisp-mode lisp-mode
+                                                     clojure-mode    scheme-mode
+                                                     haskell-mode    enh-ruby-mode
+                                                     rspec-mode      python-mode
+                                                     c-mode          c++-mode
+                                                     objc-mode       latex-mode
+                                                     plain-tex-mode))
+                (let ((mark-even-if-inactive transient-mark-mode))
+                  (indent-region (region-beginning) (region-end) nil))))))
+
+(provide '31-ccmode)
+;;; 31-ccmode.el ends here
